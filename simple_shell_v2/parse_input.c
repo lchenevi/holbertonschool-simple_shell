de446599bd0221eca8040d1c4acc93343be6e180
lchenevi <leo.cheneviere@gmail.com>
2023-12-19 14:57:43 +0100

    first push of some comments in the parse_input.c

--- simple_shell_v2/parse_input.c
+++ simple_shell_v2/parse_input.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * parse_input - Parse user input into command and arguments.
+ * parse_input - AnalyseAndSplit user input into command and arguments.
  * @input: User input string.
  * @command: Pointer to store the command.
  * @args: Array to store arguments.
@@ -11,12 +11,13 @@ void parse_input(char *input, char *command, char *args[])
 	char *token = strtok(input, " ");
 	int index;
 
+	/*Check if the string is NULL*/
 	if (token == NULL)
 	{
 		command = NULL;
 		return;
 	}
-
+	/*Copies the string in 'command' into 'token'*/
 	strcpy(command, token);
 
 	index = 0;
