886d2c8004a306f0001ab8a1f1622f5a68551cd4
lchenevi <leo.cheneviere@gmail.com>
2023-12-19 14:43:16 +0100

    commenting the interactive.c file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# holbertonschool-simple_shell
+this is my readme
--- simple_shell_v2/interactive.c
+++ simple_shell_v2/interactive.c
@@ -7,6 +7,7 @@ void interactive(void)
 	char *args[MAX_ARGS];
 	size_t input_length;
 
+	/*Checks if user is in interactive mode*/
 	if (!isatty(STDIN_FILENO))
 	{
 		fprintf(stderr, "Error: Not in interactive mode.\n");
@@ -15,8 +16,10 @@ void interactive(void)
 
 	while (1)
 	{
+		/*Prompt*/
 		printf("$ ");
 
+		/*Checks if input in correct*/
 		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
 		{
 			printf("\n");
@@ -25,27 +28,35 @@ void interactive(void)
 
 		input_length = strlen(input);
 
+		/*Check if last char isn't NULL*/
 		if (input_length > 0 && input[input_length - 1] == '\n')
 		{
 			input[input_length - 1] = '\0';
 		}
 
+		/*Compares if input is exit*/
 		if (strcmp(input, "exit") == 0)
 		{
 			break;
 		}
+		/*Compares if input is env*/
 		else if (strcmp(input, "env") == 0)
 		{
 			def_env();
 		}
+		/*Tokenizes the input string to separate the command and any
+		potential arguments*/
 		else
 		{
 			command = strtok(input, " ");
+			/*Checks if strtok successfully found the first token*/
 			if (command != NULL)
 			{
 				parse_input(input, command, args);
+				/*Analyse the input*/
 
 				execute_cmd(command, args);
+				/*Execution of the command with its arguments*/
 			}
 		}
 	}
