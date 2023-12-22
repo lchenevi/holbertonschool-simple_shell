2f54825e928c0009d18c842d8906b7dcdbafe59e
lchenevi <leo.cheneviere@gmail.com>
2023-12-22 20:50:03 +0100

    this new execute_cmd file already works, final modification for now

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,52 +1,138 @@
 #include "shell.h"
 
 /**
- * execute_cmd - executes the command given with its arguments
- * @command: User input string.
- * @args: Array to store arguments.
- *
- * This function creates a child process using fork()
+ * check_for_file - checks if file exists
+ * @filename: name of the file
+ * Return: 1 SUCCESS
+ */
+int check_for_file(char *filename)
+{
+	/*Structure to store file information*/
+	struct stat filestats;
+	/*Use the stat function to retrieve information about the file.*/
+	if (stat(filename, &filestats) >= 0)
+	{
+		/*Check if the file is executable by the owner, group, or others.*/
+		if (filestats.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH))
+			return (1); /*Return 1 = SUCCESS*/
+	}
+	return (0); /*If not, return 0*/
+}
+
+/**
+ * get_command - gets the command
+ * @path: PATH variable
+ * @command: name of the command
+ * Return: Command if SUCCESS
+ */
+char *get_command(char *path, char *command)
+{
+	char *token = NULL;
+	char *full_path = NULL;
+	/*Check if the command is an absolute or relative path.*/
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (check_for_file(command))
+			return (strdup(command));
+		else
+			return (NULL);
+	}
+	/*Tokenize the PATH variable and search for the command.*/
+	token = strtok(path, ":");
+	while (token != NULL)
+	{
+		full_path = malloc(strlen(token) + strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			return (NULL);
+		}
+		strcpy(full_path, token); /*Copy the directory path into the full path.*/
+		strcat(full_path, "/"); /*Concatenate "/" to the end of full_path.*/
+		strcat(full_path, command); /*Concatenate command name to the end*/
+		if (check_for_file(full_path))
+		{
+			return (full_path);
+		}
+		free(full_path);
+		full_path = NULL;
+		token = strtok(NULL, ":"); /*Move to next directory*/
+	}
+	return (NULL); /*Return NULL if the command is not found*/
+}
+
+/**
+ * execute_cmd - command executor
+ * @command: command to execute
+ * @args: arguments
+ * @data: data struct to store values like exit values
+ * Return: void
+ */
+int execute_cmd(char *command, char *args[], data_t *data)
+{
+	int result; /*Index values*/
+	int i;
+	char *path = NULL; /*Declaring a PATH variable*/
+
+	data->exit_status = 0; /*Setting exit_status to 0*/
+
+	path = _getenv("PATH", environ); /*Getting PATH data in a string*/
+
+	if (path == NULL)
+	{	/*If PATH not found = ERROR*/
+		fprintf(stderr, "PATH variable not found\n");
+		exit(EXIT_FAILURE);
+	}
+	/*Search the command and execute it*/
+	result = search_and_exec(command, args, path, data);
+	/*If command not executed = ERROR*/
+	free(path);
+	if (result == -1)
+		exit(EXIT_FAILURE);
+	data->exit_status = result; /*Set exit status in the data struct*/
+	for (i = 0; args[i]; i++)
+	{
+		free(args[i]);
+		args[i] = NULL;
+	}
+	/*Return exit status*/
+	return (result);
+}
+
+/**
+ * search_and_exec - search for the executable in directories and PATH
+ * @command: command to execute
+ * @args: arguments
+ * @path: PATH variable
+ * @data: data struct to store values like exit values
  * Return: 0 on success, -1 on failure
  */
-
-int execute_cmd(char *command, char *args[])
+int search_and_exec(char *command, char *args[], char *path, data_t *data)
 {
-	int result, status;
-	pid_t pid;
-
-	/*Create a child process using fork()*/
-	pid = fork();
-
-	/*Check for fork() failure*/
-	if (pid == -1)
+	int result = -1; /*Result of the search and execution.*/
+	int status = 0; /*Status of the child process.*/
+	char *full_command = NULL;
+	/*Getting full command with get_command*/
+	full_command = get_command(path, command);
+	if (full_command == NULL)
 	{
-		perror("fork");
-		return (-1);
+		fprintf(stderr, "%s: %d: %s: not found\n", data->argv[0],
+				data->command_count, command);
+		return (127);
+	} /*If command exists, call a fork and execute the command with args*/
+	else if (!fork() && execve(full_command, args, environ) == -1)
+	{	/*If execute failure, print error and set exit to 127*/
+		fprintf(stderr, "%s: %d: %s: not found\n", data->argv[0],
+				data->command_count, command);
+		data->exit_status = 127;
+		result = 127;
 	}
-	/*Child process*/
-	else if (pid == 0)
-	{
-		/*Attempt to execute the command with the provided arguments*/
-		result = execve(command, args, NULL);
-
-		/*Check if execve() failed*/
-		if (result == -1)
-		{
-			perror("execve");
-			/*Terminate the child process with failure status*/
-			_exit(EXIT_FAILURE);
-		}
-	}
-	/*Parent process*/
 	else
 	{
-		/*Wait for the child process to complete its execution*/
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			perror("waitpid");
-			return (-1);
-		}
+		wait(&status); /*Parent process waits for the child to finish.*/
+		data->exit_status = status / 256; /*Set exit_status in the data struct*/
+		result = status / 256;/*Set exit_status in result.*/
 	}
-	/*Return 0 to indicate successful execution of the command*/
-	return (0);
+	free(full_command);
+	return (result);
 }
