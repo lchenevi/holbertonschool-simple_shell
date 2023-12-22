3a28f21a69be9007c65684e1c3f0ce515d200b3a
lchenevi <leo.cheneviere@gmail.com>
2023-12-22 20:44:19 +0100

    pushing the final parse_input.c file

--- parse_input.c
+++ parse_input.c
@@ -1,54 +1,51 @@
 #include "shell.h"
 
 /**
- * parse_input - AnalyseAndSplit user input into command and arguments.
+ * parse_input - Parse user input into command and arguments.
  * @input: User input string.
  * @command: Pointer to store the command.
  * @args: Array to store arguments.
- * This function takes a user input string and tokenizes it to extract
- * the command and its arguments.
  */
-
 void parse_input(char *input, char *command, char *args[])
 {
-	/*Tokenize the input string using space as delimiter*/
-	char *token = strtok(input, " ");
-	/*Index variable for the args*/
-	int index;
-
-	(void)command;/*Suppress unused parameter warning*/
-
-	/*Check if the string is NULL*/
+	char *input_copy = strdup(input); /*Make a copy of the input string.*/
+	int index = 1; /*Index for arguments in the args array.*/
+	char *token;
+	/*Check for memory allocation failure.*/
+	if (input_copy == NULL)
+	{
+		perror("strdup");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(input_copy, " "); /*Tpkenizing with ' ' as delimiter*/
 	if (token == NULL)
 	{
-		command = NULL;
+		command[0] = '\0'; /*If 1st token is NULL, set cmd to an empty string*/
+		free(input_copy);
 		return;
 	}
-	/*Copies the string in 'token' into 'command'*/
-	/*strcpy(command, token);*/
-	index = 0;
-
-	/*Iterate through the tokens to extract arguments*/
-	while (token != NULL)
+	strcpy(command, token); /*Copy the first token to the command variable.*/
+	args[0] = strdup(token); /*Duplicate 1st token, store it in the args array.*/
+	if (args[0] == NULL)
 	{
-		/*Allocate memory for each argument and store it in the args array*/
-		args[index] = malloc(strlen(token) + 1);
-
-		/*Check if memory allocation was successful*/
-		if (args[index] == NULL)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
-		/*Copy the token (argument) to the corresponding position in the*/
-		/*args array*/
-		strcpy(args[index], token);
-
-		/*Get the next token (argument)*/
-		token = strtok(NULL, " ");
-		/*Move to the next position in the args array*/
-		index++;
+		perror("malloc/strdup");
+		exit(EXIT_FAILURE);
 	}
-	args[index] = NULL;
-	/*execute_cmd(command, args);*/
+	while (token != NULL && index < MAX_ARGS - 1) /*Continue tokenizing*/
+	{
+		token = strtok(NULL, " ");
+		if (token != NULL) /*Token isnt NULL, duplicate and store in args array.*/
+		{
+			args[index] = strdup(token);
+
+			if (args[index] == NULL) /*Check for memory allocation failure.*/
+			{
+				perror("malloc/strdup");
+				exit(EXIT_FAILURE);
+			}
+			index++;
+		}
+	}
+	args[index] = NULL; /*Last element of args array to NULL, indicating end*/
+	free(input_copy);
 }
